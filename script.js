5e38426fc81cdc5c4ea38662a2efe13c91062f18
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-02-12 16:16:21 +0100

    Take input using input slider

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const create = document.querySelector('#create');
 const canvas = document.querySelector('.canvas');
 const clearCanvas = document.querySelector('#clear');
 
+// Take input using slider
+const slider = document.querySelector('#slider');
+const output = document.querySelector('#sliderValue');
 
 
-
-create.addEventListener('click', function () {
-    let boxes = parseInt(prompt('Number of Boxes'));
+slider.addEventListener("input", function () {
     canvas.innerHTML = " ";
-    canvas.style.setProperty("--numBoxes", boxes);
-    for (let i = 0; i < boxes; i++) {
-        for (let j = 0; j < boxes; j++) {
+    let inputValue = parseInt(this.value);
+    output.textContent = `${this.value} x ${this.value}`;
+    canvas.style.setProperty("--numBoxes", inputValue);
+    for (let i = 0; i < inputValue; i++) {
+        for (let j = 0; j < inputValue; j++) {
             const div = document.createElement('div');
             div.classList.add('box');
             canvas.appendChild(div);
         }
     }
-    // Add event listener for each box
+
+    // Paint when mouse hover each boxes
     const boxElements = document.querySelectorAll('.box');
     boxElements.forEach(box => {
         box.addEventListener('mouseenter', function () {
@@ -24,6 +28,7 @@ create.addEventListener('click', function () {
         });
     });
 })
+
 
 clearCanvas.addEventListener('click', function () {
     const boxElements = document.querySelectorAll('.box');
